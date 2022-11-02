4978be6ecc9917c240ddd93dbd80c2ac309feb36
amirpourastarabadi <amirpourastarabadi@gmail.com>
2022-11-02 09:32:22 +0330

    start 2

--- resources/views/layouts/question_types/fill_place_holder_question.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="question row my-4 h3 mb-5">
-    @foreach ($blank_questions as $question)
-    <p class="col-12 text mb-3 bg-light me-3" style="line-height: 46px; text-align: justify">
-        {{$loop->iteration . '- ' . $question->body}}
-    </p>
-    <hr>
-    @endforeach
-</div>
--- routes/student.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\Student\ExamController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/exams/{exam}', [ExamController::class, 'show'])->name('exams.show');
-Route::post('/exams/{exam}', [ExamController::class, 'submit'])->name('exams.submit');
